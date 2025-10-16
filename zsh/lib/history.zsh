ef5cc549e68df97ee7e0482fbe26ff0650425b05
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-10-16 19:30:16 +0900

    feat: zshのHistory設定

---
+++ zsh/lib/history.zsh
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------
+# HISTORY
+# ------------------------------------------------------------------------------
+
+HISTFILE=$HOME/.zsh_history     # 履歴を保存するファイル
+HISTSIZE=100000                 # メモリ上に保存する履歴のサイズ
+SAVEHIST=1000000                # 上述のファイルに保存する履歴のサイズ
+
+# share .zshhistory
+setopt inc_append_history       # 実行時に履歴をファイルにに追加していく
+setopt share_history            # 履歴を他のシェルとリアルタイム共有する
+
+setopt hist_ignore_all_dups     # ヒストリーに重複を表示しない
+setopt hist_save_no_dups        # 重複するコマンドが保存されるとき、古い方を削除する。
+setopt extended_history         # コマンドのタイムスタンプをHISTFILEに記録する
+setopt hist_expire_dups_first   # HISTFILEのサイズがHISTSIZEを超える場合は、最初に重複を削除します
+setopt inc_append_history       # 実行時に履歴をファイルにに追加していく
+setopt share_history            # 履歴を他のシェルとリアルタイム共有する
