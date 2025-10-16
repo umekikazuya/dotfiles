47bd64a96fe45e50945445e1e461d09e6f5aed69
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-10-16 19:19:55 +0900

    feat: .zshrcファイルの追加

---
+++ zsh/.zshrc
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------------
+# MAIN .zshrc
+# ------------------------------------------------------------------------------
+#
+# This file sources all the config files in the `lib` directory.
+#
+# ------------------------------------------------------------------------------
+
+# Define the lib directory, assuming .zshrc is in the zsh directory of the dotfiles repo
+ZSH_LIB_DIR="${ZDOTDIR:-$HOME/.dotfiles}/zsh/lib"
+
+# Source all .zsh files in the lib directory
+for file in "$ZSH_LIB_DIR"/*.zsh; do
+  if [ -r "$file" ]; then
+    source "$file"
+  fi
+done
+
+# Load local settings if they exist
+if [ -f "${HOME}/.zshrc.local" ]; then
+  source "${HOME}/.zshrc.local"
+fi
+
+unset ZSH_LIB_DIR file
---
+++ zsh/lib/aliases.zsh
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+# ALIASES
+# ------------------------------------------------------------------------------
+
+# Git
+alias g='git'
+
+# Docker
+alias d='docker'
+alias dc='docker compose'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias dst='docker stats'
+alias dil='docker images'
+alias dlog='docker compose logs -f'
+alias dex='docker exec -it'
+alias drm='docker rm'
+alias dri='docker rmi'
+alias dcl='docker system df && docker volume ls -qf dangling=true | xargs -r docker volume rm && docker images -f "dangling=true" -q | xargs -r docker rmi && docker container prune --force --filter "until=168h" && docker ps --filter "status=exited" | grep "weeks ago" | awk "{print \$1}" | xargs -r docker rm && docker builder prune && docker system df'
+
+# ssh config のホスト名一覧表示
+alias sshli="cat ~/.ssh/config |grep ^Host\  |sed -e 's/^Host\ //g'"
