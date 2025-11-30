331bd081377cb14e6b39a39dde3c7a8b6859eac9
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-11-30 16:39:16 +0900

    feat: zsh-historyの設定を更新

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -9,6 +9,8 @@ SAVEHIST=1000000                # 上述のファイルに保存する履歴の�
 # share .zshhistory
 setopt inc_append_history       # 実行時に履歴をファイルにに追加していく
 setopt share_history            # 履歴を他のシェルとリアルタイム共有する
+setopt hist_verify              # 履歴を呼び出す時に実行前の内容を一度表示する
+
 
 setopt hist_ignore_all_dups     # ヒストリーに重複を表示しない
 setopt hist_save_no_dups        # 重複するコマンドが保存されるとき、古い方を削除する。
