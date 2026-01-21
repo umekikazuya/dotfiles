f56d7c3901f31ecebe19f451b7bb78db0503d170
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-01-21 09:48:45 +0900

    feat: neovimにてphpのlintとformatを設定
    * プロジェクトのDockerコンテナのCIツールを利用するよう設定
    * .inc, .module, .themeをPHP拡張子として扱うよう設定

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,8 +1,30 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      php = { "phpcbf" },
-    },
-  },
+  opts = function(_, opts)
+    -- プロジェクト毎の環境変数にてサービス名を設定する
+    local service = os.getenv("NVIM_PHP_DOCKER_SERVICE") or "app"
+
+    opts.default_format_opts = opts.default_format_opts or {}
+    opts.default_format_opts.timeout_ms = 30000
+
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters_by_ft.php = { "phpcbf" }
+
+    opts.formatters = opts.formatters or {}
+    opts.formatters.phpcbf = {
+      command = "docker",
+      args = {
+        "compose",
+        "exec",
+        "-T",
+        service,
+        "php",
+        "vendor/bin/phpcbf",
+        "-",
+      },
+      stdin = true,
+    }
+
+    return opts
+  end,
 }
--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -1,8 +1,27 @@
 return {
   "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      php = { "phpstan" },
-    },
-  },
+  opts = function(_, opts)
+    local service = os.getenv("NVIM_PHP_DOCKER_SERVICE") or "app"
+
+    opts.linters_by_ft = opts.linters_by_ft or {}
+    opts.linters_by_ft.php = { "phpcs" }
+
+    opts.linters = opts.linters or {}
+    opts.linters.phpcs = {
+      cmd = "docker",
+      args = {
+        "compose",
+        "exec",
+        "-T",
+        service,
+        "php",
+        "vendor/bin/phpcs",
+        "--report=json",
+        "-",
+      },
+      stdin = true,
+    }
+
+    return opts
+  end,
 }
