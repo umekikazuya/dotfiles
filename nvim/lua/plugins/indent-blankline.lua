fc30c87ce5558af662adabcb18f3a600286cff23
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-03-01 00:21:25 +0900

    feat: nvimのインデントの色を更新

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,35 +2,24 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   lazy = true,
   event = { "BufReadPost", "BufNewFile" },
-
   main = "ibl",
-  ---@module "ibl"
-  ---@type ibl.config
-  config = function()
-    local highlight = {
-      "Red",
-      "Orange",
-      "Yellow",
-      "Green",
-      "Blue",
-      "Purple",
-    }
+  opts = function()
     local hooks = require("ibl.hooks")
-    -- create the highlight groups in the highlight setup hook, so they are reset
-    -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "Write", { fg = "#E2E1DF" })
-      vim.api.nvim_set_hl(0, "Red", { fg = "#E2E1DF" })
-      vim.api.nvim_set_hl(0, "Orange", { fg = "#E2E1DF" })
-      vim.api.nvim_set_hl(0, "Yellow", { fg = "#E2E1DF" })
-      vim.api.nvim_set_hl(0, "Green", { fg = "#E2E1DF" })
-      vim.api.nvim_set_hl(0, "Blue", { fg = "#E2E1DF" })
-      vim.api.nvim_set_hl(0, "Purple", { fg = "#E2E1DF" })
+      -- kanso-pearlの背景に近い、控えめなグレーのグラデーション
+      vim.api.nvim_set_hl(0, "IblIndent1", { fg = "#D1D0CC" })
+      vim.api.nvim_set_hl(0, "IblIndent2", { fg = "#C2C1BD" })
+      vim.api.nvim_set_hl(0, "IblScope", { fg = "#8b5cf6", bold = true }) -- スコープ中の線は少し強調
     end)
 
-    require("ibl").setup({
-      indent = { char = "▏", highlight = highlight },
-      scope = { highlight = "Write" },
-    })
+    return {
+      indent = {
+        char = "▏",
+        highlight = { "IblIndent1", "IblIndent2" },
+      },
+      scope = {
+        highlight = "IblScope",
+      },
+    }
   end,
 }
