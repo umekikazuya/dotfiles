36468e1a9c04de494c047c5865d24c5f7013193f
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-03-01 00:31:42 +0900

    feat: lualine設定

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function()
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "snacks_dashboard" } },
+        component_separators = { left = "│", right = "│" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", icon = "💻" } },
+        lualine_b = { { "branch", icon = "" } },
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path() },
+        },
+        lualine_y = {
+          {
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.command.has()
+            end,
+          },
+          { "diagnostics" },
+          { "diff" },
+        },
+        lualine_z = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+      },
+    }
+  end,
+}
