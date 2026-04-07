7a22cb7200adbd381d39e08fbeaa9054700cd7be
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-04-07 15:25:07 +0900

    feat: nvimのmise PATHの設定記述箇所を変更

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
