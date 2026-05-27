dc66f18ebe99b2d02814ca99317ab9e3d1a65237
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-05-27 20:59:45 +0900

    nvim: dart 開発のために lazyvim の extras を追加

--- nvim/lua/config/extras.lua
+++ nvim/lua/config/extras.lua
@@ -1,6 +1,7 @@
 return {
   -- Lang
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.dart" },
 
   -- Database
   { import = "lazyvim.plugins.extras.lang.sql" },
