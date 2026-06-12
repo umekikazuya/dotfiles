dfcfae221e309a8238d1ffac2437c66061d59145
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-06-12 17:23:04 +0900

    nvim: prettier の設定

---
+++ nvim/lua/plugins/prettier.lua
@@ -0,0 +1,79 @@
+---@alias ConformCtx {buf: number, filename: string, dirname: string}
+local M = {}
+
+local supported = {
+  "css",
+  "graphql",
+  "handlebars",
+  "html",
+  "javascript",
+  "javascriptreact",
+  "json",
+  "jsonc",
+  "less",
+  "markdown",
+  "markdown.mdx",
+  "scss",
+  "typescript",
+  "typescriptreact",
+  "vue",
+  "yaml",
+}
+
+---@param ctx ConformCtx
+function M.has_config(ctx)
+  vim.fn.system({ "prettier", "--find-config-path", ctx.filename })
+  return vim.v.shell_error == 0
+end
+
+---@param ctx ConformCtx
+function M.has_parser(ctx)
+  local ft = vim.bo[ctx.buf].filetype --[[@as string]]
+  if vim.tbl_contains(supported, ft) then
+    return true
+  end
+  local ret = vim.fn.system({ "prettier", "--file-info", ctx.filename })
+  ---@type boolean, string?
+  local ok, parser = pcall(function()
+    return vim.fn.json_decode(ret).inferredParser
+  end)
+  return ok and parser and parser ~= vim.NIL
+end
+
+M.has_config = LazyVim.memoize(M.has_config)
+M.has_parser = LazyVim.memoize(M.has_parser)
+
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "prettier" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    ---@param opts ConformOpts
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      for _, ft in ipairs(supported) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "prettier")
+      end
+
+      opts.formatters = opts.formatters or {}
+      opts.formatters.prettier = {
+        condition = function(_, ctx)
+          return M.has_parser(ctx) and (vim.g.lazyvim_prettier_needs_config ~= true or M.has_config(ctx))
+        end,
+      }
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.prettier)
+    end,
+  },
+}
