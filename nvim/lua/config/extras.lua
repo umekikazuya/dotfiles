1c6e061534125b5c20ea7ff082755272a2428106
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-06-23 00:26:08 +0900

    nvim: extras から dart, sql を削除

--- nvim/lua/config/extras.lua
+++ nvim/lua/config/extras.lua
@@ -1,8 +1,4 @@
 return {
   -- Lang
   { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.dart" },
-
-  -- Database
-  { import = "lazyvim.plugins.extras.lang.sql" },
 }
