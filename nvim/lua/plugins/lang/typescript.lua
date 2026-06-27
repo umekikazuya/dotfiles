4ca4cc1aaaf24d0e25e04ec07856023a12ac8efa
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-06-28 02:53:44 +0900

    mise|nvim: vtsls を追加、LSPを vtsls に変更。

--- nvim/lua/plugins/lang/typescript.lua
+++ nvim/lua/plugins/lang/typescript.lua
@@ -6,22 +6,43 @@ return {
       vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "javascript" })
     end,
   },
-
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        ts_ls = {},
+        vtsls = {
+          root_dir = function(fname)
+            local util = require("lspconfig.util")
+            return util.root_pattern("tsconfig.app.json", "tsconfig.json", "package.json")(fname)
+          end,
+          settings = {
+            vtsls = {
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                completion = { enableServerSideFuzzyMatch = true },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = { completeFunctionCalls = true },
+              inlayHints = {
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+            },
+          },
+        },
       },
     },
   },
-
   {
     "windwp/nvim-ts-autotag",
     event = "VeryLazy",
     opts = {},
   },
-
   {
     "folke/ts-comments.nvim",
     event = "VeryLazy",
