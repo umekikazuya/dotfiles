0ae0d49a90c405e6977e9a4e914916380ada75a1
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-07-02 21:40:56 +0900

    nvim: fzf-lua を導入

---
+++ nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,10 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "mini.icons/mini.icons" },
+  ---@module "fzf-lua"
+  ---@type fzf-lua.Config|{}
+  ---@diagnostic disable: missing-fields
+  opts = {},
+  ---@diagnostic enable: missing-fields
+}
