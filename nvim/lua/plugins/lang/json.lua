aea5e60499c117b7a18daadbeeeb2aaf61ed6048
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-07-02 21:38:26 +0900

    mise|nvim: json のLSPをmise管理

--- nvim/lua/plugins/lang/json.lua
+++ nvim/lua/plugins/lang/json.lua
@@ -11,10 +11,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      -- make sure mason installs the server
       servers = {
         jsonls = {
-          -- lazy-load schemastore when needed
+          mason = false,
+          cmd = { "vscode-json-languageserver", "--stdio" },
           before_init = function(_, new_config)
             new_config.settings.json.schemas = new_config.settings.json.schemas or {}
             vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
