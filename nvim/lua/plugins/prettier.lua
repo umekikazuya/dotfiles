004d5da932e81c845743d95a14a42f4e61cd14c5
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-07-08 00:05:40 +0900

    nvim: prettier 設定

--- nvim/lua/plugins/prettier.lua
+++ nvim/lua/plugins/prettier.lua
@@ -20,6 +20,24 @@ local supported = {
   "yaml",
 }
 
+local config_names = {
+  ".prettierrc",
+  ".prettierrc.json",
+  ".prettierrc.json5",
+  ".prettierrc.yml",
+  ".prettierrc.yaml",
+  ".prettierrc.js",
+  ".prettierrc.cjs",
+  ".prettierrc.mjs",
+  ".prettierrc.toml",
+  "prettier.config.js",
+  "prettier.config.cjs",
+  "prettier.config.mjs",
+  "prettier.config.ts",
+  "prettier.config.cts",
+  "prettier.config.mts",
+}
+
 local function memoize(fn)
   local cache = {}
   return function(ctx)
@@ -32,25 +50,22 @@ local function memoize(fn)
 end
 
 ---@param ctx ConformCtx
+function M.has_local_prettier(ctx)
+  return vim.fs.find("node_modules/.bin/prettier", { path = ctx.dirname, upward = true })[1] ~= nil
+end
+
+---@param ctx ConformCtx
 function M.has_config(ctx)
-  vim.fn.system({ "prettier", "--find-config-path", ctx.filename })
-  return vim.v.shell_error == 0
+  return vim.fs.find(config_names, { path = ctx.dirname, upward = true })[1] ~= nil
 end
 
 ---@param ctx ConformCtx
 function M.has_parser(ctx)
   local ft = vim.bo[ctx.buf].filetype --[[@as string]]
-  if vim.tbl_contains(supported, ft) then
-    return true
-  end
-  local ret = vim.fn.system({ "prettier", "--file-info", ctx.filename })
-  ---@type boolean, string?
-  local ok, parser = pcall(function()
-    return vim.fn.json_decode(ret).inferredParser
-  end)
-  return ok and parser and parser ~= vim.NIL
+  return vim.tbl_contains(supported, ft)
 end
 
+M.has_local_prettier = memoize(M.has_local_prettier)
 M.has_config = memoize(M.has_config)
 M.has_parser = memoize(M.has_parser)
 
@@ -68,7 +83,7 @@ return {
       opts.formatters = opts.formatters or {}
       opts.formatters.prettier = {
         condition = function(_, ctx)
-          return M.has_config(ctx) and M.has_parser(ctx)
+          return M.has_local_prettier(ctx) and M.has_config(ctx) and M.has_parser(ctx)
         end,
       }
     end,
