7dfff3441ae64f591eb2c23e52199671f6c34c4b
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-07-09 08:47:22 +0900

    nvim: render-markdown のsnacksでのトグル設定を削除
    ```
    :lua require("render-markdown").set(true)                                                                         ┃
    :lua require("render-markdown").set(false)                                                                        ┃
    ```

--- nvim/lua/plugins/lang/markdown.lua
+++ nvim/lua/plugins/lang/markdown.lua
@@ -93,11 +93,6 @@ return {
     ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
     config = function(_, opts)
       require("render-markdown").setup(opts)
-      Snacks.toggle({
-        name = "Render Markdown",
-        get = require("render-markdown").get,
-        set = require("render-markdown").set,
-      }):map("<leader>um")
     end,
   },
 }
