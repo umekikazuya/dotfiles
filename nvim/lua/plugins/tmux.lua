c661df366e4087f66c62097711da8157f94d725b
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-07-12 01:15:14 +0900

    nvim: vim-tmux-navigator のデフォルトキーマップを無効化

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,6 +1,9 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
     cmd = {
       "TmuxNavigateLeft",
       "TmuxNavigateDown",
