e90d7157a94f5f0e9c15abded609565f9185ba05
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-07-12 01:04:03 +0900

    nvim: 不要なパッケージの削除

--- nvim/lua/plugins/snacks.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "folke/snacks.nvim",
-  enabled = false,
-}
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -25,6 +25,58 @@ return {
       },
     },
   },
-  { "folke/noice.nvim", enabled = false }, -- TODO: 削除対象
-  { "akinsho/bufferline.nvim", enabled = false }, -- TODO: 削除対象
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function()
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "snacks_dashboard" } },
+        component_separators = { left = " ", right = " " },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            color = {
+              gui = "italic",
+              fg = "#cccccc",
+            },
+            fmt = function(str)
+              return str
+            end,
+            padding = { left = 1, right = 1 },
+          },
+        },
+        lualine_b = { { "branch" } },
+        lualine_c = {
+          {
+            "filename",
+            padding = { left = 1, right = 1 },
+            path = 1,
+            symbols = {
+              modified = " ●",
+              readonly = " ",
+              unnamed = "[No Name]",
+            },
+            color = { fg = "#3b3248", bg = "#cccccc", gui = "italic,bold" },
+          },
+        },
+        lualine_y = {
+          { "diagnostics" },
+        },
+        lualine_z = {
+          {
+            function()
+              return vim.fn.line(".") .. "/" .. vim.fn.line("$")
+            end,
+            padding = { left = 1, right = 1 },
+            color = { fg = "#3b3248", bg = "#ece8df" },
+          },
+        },
+      },
+    }
+  end,
 }
