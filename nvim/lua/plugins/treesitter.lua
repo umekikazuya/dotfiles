053635823277c1f8220254e12a7539d0c5927e5d
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-07-18 23:50:06 +0900

    nvim: treesitter 設定

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,28 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    },
-  },
-}
+vim.pack.add({
+  "https://github.com/nvim-treesitter/nvim-treesitter.git",
+}, { confirm = false })
+
+local ts = require("nvim-treesitter")
+
+ts.install({
+  "dockerfile",
+  "go",
+  "gomod",
+  "gosum",
+  "gowork",
+  "javascript",
+  "json5",
+  "php",
+  "tsx",
+  "typescript",
+  "yaml",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("my.treesitter", { clear = true }),
+  callback = function(ev)
+    if pcall(vim.treesitter.start, ev.buf) then
+      vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+  end,
+})
