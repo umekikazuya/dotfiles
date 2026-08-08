3fb298697d091399de9368e16efd11e36ee0fbd4
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-08-09 02:02:16 +0900

    nvim: 診断系のUIを設定

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -26,10 +26,10 @@ vim.diagnostic.config({
   virtual_text = { spacing = 2, source = "if_many", prefix = "●" },
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN] = " ",
-      [vim.diagnostic.severity.HINT] = " ",
-      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.ERROR] = "E",
+      [vim.diagnostic.severity.WARN] = "W",
+      [vim.diagnostic.severity.HINT] = "H",
+      [vim.diagnostic.severity.INFO] = "I",
     },
   },
 })
