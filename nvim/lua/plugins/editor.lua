80cc59f3baafe38946641e2898adefd37a070167
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-08-09 02:02:37 +0900

    nvim: flash 設定を見直し

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -17,16 +17,15 @@ local flash = require("flash")
 flash.setup({})
 -- stylua: ignore start
 vim.keymap.set({ "n", "x", "o" }, "s", function() flash.jump() end, { desc = "Flash" })
-vim.keymap.set({ "n", "o", "x" }, "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
 vim.keymap.set("o", "r", function() flash.remote() end, { desc = "Remote Flash" })
 vim.keymap.set({ "o", "x" }, "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
 vim.keymap.set("c", "<c-s>", function() flash.toggle() end, { desc = "Toggle Flash Search" })
 -- stylua: ignore end
-vim.keymap.set({ "n", "o", "x" }, "<c-space>", function()
+vim.keymap.set({ "n", "o", "x" }, "<M-space>", function()
   flash.treesitter({
     actions = {
-      ["<c-space>"] = "next",
-      ["<BS>"] = "prev",
+      ["<M-n>"] = "next",
+      ["<M-p>"] = "prev",
     },
   })
 end, { desc = "Treesitter Incremental Selection" })
