d14058e69563b32e5ca124b71b14c4ccc8edf316
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-08-09 01:54:22 +0900

    nvim: yanky のキーマップを見直し

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -10,23 +10,10 @@ require("yanky").setup({
 })
 
 local map = vim.keymap.set
-map({ "n", "x" }, "<leader>p", "<cmd>YankyRingHistory<cr>", { desc = "Open Yank History" })
 -- stylua: ignore start
 map({ "n", "x" }, "y", "<Plug>(YankyYank)", { desc = "Yank Text" })
 map({ "n", "x" }, "p", "<Plug>(YankyPutAfter)", { desc = "Put Text After Cursor" })
 map({ "n", "x" }, "P", "<Plug>(YankyPutBefore)", { desc = "Put Text Before Cursor" })
-map({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)", { desc = "Put Text After Selection" })
-map({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)", { desc = "Put Text Before Selection" })
 map("n", "<C-p>", "<Plug>(YankyCycleForward)", { desc = "Cycle Forward Through Yank History" })
 map("n", "<C-n>", "<Plug>(YankyCycleBackward)", { desc = "Cycle Backward Through Yank History" })
-map("n", "]p", "<Plug>(YankyPutIndentAfterLinewise)", { desc = "Put Indented After Cursor (Linewise)" })
-map("n", "[p", "<Plug>(YankyPutIndentBeforeLinewise)", { desc = "Put Indented Before Cursor (Linewise)" })
-map("n", "]P", "<Plug>(YankyPutIndentAfterLinewise)", { desc = "Put Indented After Cursor (Linewise)" })
-map("n", "[P", "<Plug>(YankyPutIndentBeforeLinewise)", { desc = "Put Indented Before Cursor (Linewise)" })
-map("n", ">p", "<Plug>(YankyPutIndentAfterShiftRight)", { desc = "Put and Indent Right" })
-map("n", "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", { desc = "Put and Indent Left" })
-map("n", ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", { desc = "Put Before and Indent Right" })
-map("n", "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", { desc = "Put Before and Indent Left" })
-map("n", "=p", "<Plug>(YankyPutAfterFilter)", { desc = "Put After Applying a Filter" })
-map("n", "=P", "<Plug>(YankyPutBeforeFilter)", { desc = "Put Before Applying a Filter" })
 -- stylua: ignore end
