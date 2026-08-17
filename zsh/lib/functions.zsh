c768dc522b17dec8048b5049cb8688843350e17f
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-08-18 04:00:56 +0900

    nvim: `sw`関数でアクティブセッションとセッション未作成でリスト順を分けるよう設定

--- zsh/lib/functions.zsh
+++ zsh/lib/functions.zsh
@@ -1,6 +1,24 @@
 # sw: ghq+fzfでプロジェクトをswitch
 function sw() {
-  local selected=$(ghq list | fzf --reverse --popup 80%)
+  local selected=$(
+    {
+    while IFS= read -r repo; do
+      local session_name=$(basename "$repo" | tr . _)
+
+      if tmux has-session -t "$session_name" 2>/dev/null; then
+        printf '[active]\t%s\n' "$repo"
+      fi
+    done < <(ghq list)
+
+    while IFS= read -r repo; do
+      local session_name=$(basename "$repo" | tr . _)
+
+      if ! tmux has-session -t "$session_name" 2>/dev/null; then
+        printf '[new ]\t%s\n' "$repo"
+      fi
+    done < <(ghq list)
+    } | fzf --reverse --popup 80% --no-sort | cut -f2-
+  )
 
   if [ -n "$selected" ]; then
     local full_path="$(ghq root)/$selected"
