5c1fc8af9d2679cd2d8ae86fd0e4237d656bf4ec
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-08-18 09:50:40 +0900

    nvim: intelephense LSP設定

--- nvim/lua/plugins/lang/php.lua
+++ nvim/lua/plugins/lang/php.lua
@@ -1,2 +1,14 @@
-vim.lsp.config("intelephense", {})
+vim.lsp.config("intelephense", {
+  settings = {
+    intelephense = {
+      format = { enable = false },
+      files = {
+        maxSize = 2000000,
+      },
+      diagnostics = {
+        undefindTypes = false,
+      },
+    }
+  }
+})
 vim.lsp.enable("intelephense")
