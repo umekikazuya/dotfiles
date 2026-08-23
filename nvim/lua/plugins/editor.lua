a8365f031137a31add62353c72a9e12900858c4a
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-08-23 20:15:29 +0900

    nvim: surround のキーマップを変更
    - `sa`
    - `sr`
    - `sd`
    
    flash 設定を無効化

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,6 +1,5 @@
 vim.pack.add({
   "https://github.com/folke/trouble.nvim.git",
-  "https://github.com/folke/flash.nvim.git",
   "https://github.com/nvim-mini/mini.pairs.git",
   "https://github.com/nvim-mini/mini.surround.git",
   "https://github.com/nvim-mini/mini.ai.git",
@@ -13,35 +12,18 @@ vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<
 vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List" })
 vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List" })
 
-local flash = require("flash")
-flash.setup({})
--- stylua: ignore start
-vim.keymap.set({ "n", "x", "o" }, "s", function() flash.jump() end, { desc = "Flash" })
-vim.keymap.set("o", "r", function() flash.remote() end, { desc = "Remote Flash" })
-vim.keymap.set({ "o", "x" }, "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
-vim.keymap.set("c", "<c-s>", function() flash.toggle() end, { desc = "Toggle Flash Search" })
--- stylua: ignore end
-vim.keymap.set({ "n", "o", "x" }, "<M-space>", function()
-  flash.treesitter({
-    actions = {
-      ["<M-n>"] = "next",
-      ["<M-p>"] = "prev",
-    },
-  })
-end, { desc = "Treesitter Incremental Selection" })
-
 require("mini.pairs").setup({})
 
 require("mini.surround").setup({
   silent = true,
   mappings = {
-    add = "gsa", -- Add surrounding in Normal and Visual modes
-    delete = "gsd", -- Delete surrounding
-    find = "gsf", -- Find surrounding (to the right)
-    find_left = "gsF", -- Find surrounding (to the left)
-    highlight = "gsh", -- Highlight surrounding
-    replace = "gsr", -- Replace surrounding
-    update_n_lines = "gsn", -- Update `n_lines`
+    add = "sa",            -- Add surrounding in Normal and Visual modes
+    delete = "sd",         -- Delete surrounding
+    find = "sf",           -- Find surrounding (to the right)
+    find_left = "sF",      -- Find surrounding (to the left)
+    highlight = "sh",      -- Highlight surrounding
+    replace = "sr",        -- Replace surrounding
+    update_n_lines = "sn", -- Update `n_lines`
   },
 })
 
@@ -51,6 +33,6 @@ require("mini.ai").setup({
 
 require("treesj").setup({
   use_default_keymaps = false, -- デフォルトのキーマップ(gS/gJ)は使わず、<leader>mでトグルさせる
-  max_join_length = 1000, -- 実質無制限に近くし、長いTableテストなども1行にまとめられるようにする
+  max_join_length = 1000,      -- 実質無制限に近くし、長いTableテストなども1行にまとめられるようにする
 })
 vim.keymap.set("n", "<leader>m", "<CMD>TSJToggle<CR>", { desc = "Toggle Split/Join" })
