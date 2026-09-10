00b55ca37bf662a9768a5e2dc52ba1ea67904277
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-09-10 19:12:44 +0900

    nvim: indent-blankline 設定

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,4 +2,8 @@ vim.pack.add({
   "https://github.com/lukas-reineke/indent-blankline.nvim.git",
 }, { confirm = false })
 
-require("ibl").setup({})
+require("ibl").setup({
+  scope = {
+    enabled = true,
+  },
+})
